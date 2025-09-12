ca60e13b3b6c7cfaed615675e7ef5e824d56aeb3
Krishna Lal <krishnalal1360@gmail.com>
2025-09-12 19:24:40 +0530

    Started

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+
+/*Route::get('/', function () {
+    return view('customer.index');
+});*/
+//
+Route::resource('customers', CustomerController::class);
